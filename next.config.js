45b02fa5550f82da5c523212bbc599004decb652
dai.trinhduc <dai.trinhduc@ncc.asia>
2025-01-24 17:24:17 +0700

    update middleware

--- next.config.js
+++ next.config.js
@@ -6,21 +6,6 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*', // Apply to all routes
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          },
-          { key: 'Pragma', value: 'no-cache' },
-          { key: 'Expires', value: '0' },
-        ],
-      },
-    ];
-  },
   // output: 'standalone',
 };
 
