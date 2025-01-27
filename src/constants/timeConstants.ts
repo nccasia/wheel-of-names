5f08001deebe6cb410880142166061d1e13dbb6d
dai.trinhduc <dai.trinhduc@ncc.asia>
2025-01-27 23:04:40 +0700

    update time

--- src/constants/timeConstants.ts
+++ src/constants/timeConstants.ts
@@ -1,5 +1,5 @@
 export const TimeConstants = {
     SPIN_TIME: 5000, // 5 seconds
     SPIN_INTERVAL:  50, // 50 milliseconds
-    NEW_LUNAR_YEAR: "2025-01-21T00:00:00+07:00"
+    NEW_LUNAR_YEAR: "2025-01-29T00:00:00+07:00"
 }
