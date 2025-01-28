ab01d8c02b706a9e2041a7cf54caebff4e5d5275
dai.trinhduc <dai.trinhduc@ncc.asia>
2025-01-28 22:12:03 +0700

    update reward

--- src/constants/gameRewards.ts
+++ src/constants/gameRewards.ts
@@ -1,72 +1,127 @@
 export const GameRewards = [
-    {
-        value: 1,
-        text: '50.000 VND',
-        label: 'Bạn nhận được lì xì 50,000 VND',
-    },
-    {
-        value: 2,
-        text: '100.000 VND',
-        label: 'Bạn nhận được lì xì 100,000 VND',
-    },
-    {
-        value: 1,
-        text: '50.000 VND',
-        label: 'Bạn nhận được lì xì 50,000 VND',
-    },
-    {
-        value: 2,
-        text: '100.000 VND',
-        label: 'Bạn nhận được lì xì 100,000 VND',
-    },
-    {
-        value: 1,
-        text: '50.000 VND',
-        label: 'Bạn nhận được lì xì 50,000 VND',
-    },
-    {
-        value: 3,
-        text: '200.000 VND',
-        label: 'Bạn nhận được lì xì 200,000 VND',
-    },
-    {
-        value: 2,
-        text: '100.000 VND',
-        label: 'Bạn nhận được lì xì 100,000 VND',
-    },
-    {
-        value: 10,
-        text: '500.000 VND',
-        label: 'Bạn nhận được lì xì 500,000 VND',
-    },
-    {
-        value: 1,
-        text: '50.000 VND',
-        label: 'Bạn nhận được lì xì 50,000 VND',
-    },
-    {
-        value: 2,
-        text: '100.000 VND',
-        label: 'Bạn nhận được lì xì 100,000 VND',
-    },
-    {
-        value: 3,
-        text: '500.000 VND',
-        label: 'Bạn nhận được lì xì 200,000 VND',
-    },
-    {
-        value: 1,
-        text: '50.000 VND',
-        label: 'Bạn nhận được lì xì 50,000 VND',
-    },
-    {
-        value: 2,
-        text: '100.000 VND',
-        label: 'Bạn nhận được lì xì 100,000 VND',
-    },
-    {
-        value: 10,
-        text: '500.000 VND',
-        label: 'Bạn nhận được lì xì 500,000 VND',
-    },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
+  {
+    value: 100000,
+    text: "100.000 VND",
+    label: "Bạn nhận được lì xì 100,000 VND",
+  },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
+  {
+    value: 200000,
+    text: "200.000 VND",
+    label: "Bạn nhận được lì xì 200,000 VND",
+  },
+  {
+    value: 100000,
+    text: "100.000 VND",
+    label: "Bạn nhận được lì xì 100,000 VND",
+  },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
+  {
+    value: 200000,
+    text: "200.000 VND",
+    label: "Bạn nhận được lì xì 200,000 VND",
+  },
+  {
+    value: 100000,
+    text: "100.000 VND",
+    label: "Bạn nhận được lì xì 100,000 VND",
+  },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
+  {
+    value: 100000,
+    text: "100.000 VND",
+    label: "Bạn nhận được lì xì 100,000 VND",
+  },
+  {
+    value: 200000,
+    text: "200.000 VND",
+    label: "Bạn nhận được lì xì 200,000 VND",
+  },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
+  {
+    value: 100000,
+    text: "100.000 VND",
+    label: "Bạn nhận được lì xì 100,000 VND",
+  },
+  {
+    value: 500000,
+    text: "500.000 VND",
+    label: "Bạn nhận được lì xì 500,000 VND",
+  },
+  {
+    value: 100000,
+    text: "100.000 VND",
+    label: "Bạn nhận được lì xì 100,000 VND",
+  },
+  {
+    value: 200000,
+    text: "200.000 VND",
+    label: "Bạn nhận được lì xì 200,000 VND",
+  },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
+  {
+    value: 100000,
+    text: "100.000 VND",
+    label: "Bạn nhận được lì xì 100,000 VND",
+  },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
+  {
+    value: 100000,
+    text: "100.000 VND",
+    label: "Bạn nhận được lì xì 100,000 VND",
+  },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
+  {
+    value: 100000,
+    text: "100.000 VND",
+    label: "Bạn nhận được lì xì 100,000 VND",
+  },
+  {
+    value: 200000,
+    text: "200.000 VND",
+    label: "Bạn nhận được lì xì 200,000 VND",
+  },
+  {
+    value: 50000,
+    text: "50.000 VND",
+    label: "Bạn nhận được lì xì 50,000 VND",
+  },
 ];
